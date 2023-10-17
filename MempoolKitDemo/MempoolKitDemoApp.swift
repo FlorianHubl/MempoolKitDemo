f460a3d173ee825ab80f29d151619828119ad5f7
Florian Hubl <florian.hubl@ewe.net>
2023-10-17 22:43:54 +0200

    Initial Commit

---
+++ MempoolKitDemo/MempoolKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MempoolKitDemoApp.swift
+//  MempoolKitDemo
+//
+//  Created by Florian Hubl on 17.10.23.
+//
+
+import SwiftUI
+
+@main
+struct MempoolKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
